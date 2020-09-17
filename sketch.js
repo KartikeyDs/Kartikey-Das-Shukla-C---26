410f5883cd6b2e491268adf2de86260b2bd7d508
Kartikey Das Shukla <kartikeydasshukla9730@gmail.com>
2020-09-17 18:32:20 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform = new Ground(150,305,300,170);
+
 }
 
 function draw(){
@@ -59,4 +63,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
